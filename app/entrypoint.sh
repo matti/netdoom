6b0bd177c5fddbcb649c66426f8da37bc765b450
Matti Paksula <matti.paksula@iki.fi>
2019-09-15 22:12:23 +0300

    best

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 _term() {
   >&2 echo "TERM"
+  set +e
+    kill -9 $doom_pid
+  set -e
   exit 0
 }
 trap "_term" TERM
@@ -12,11 +15,22 @@ _err() {
   exit 1
 }
 
-Xvfb :0 -screen 0 320x240x24 &
-x11vnc -display :0 -passwd secret -forever &
+case $1 in
+  server)
+    /usr/games/chocolate-doom -dedicated &
+    doom_pid=$!
+  ;;
+  client)
+    Xvfb :0 -screen 0 320x240x24 &
+    x11vnc -display :0 -passwd secret -forever -shared -nocursor &
 
-/usr/games/chocolate-doom $@ &
-doom_pid=$!
+    /usr/games/chocolate-doom \
+      -nomouse -nosound \
+      -window -geometry 320x240 \
+      -config /app/chocolate-doom.cfg \
+      -connect server -nodes 3 &
+    doom_pid=$!
+  ;;
+esac
 
-echo "started: $@"
 wait $doom_pid
